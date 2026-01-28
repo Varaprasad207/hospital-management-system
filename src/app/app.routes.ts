16ed5263f6bebaa9d0a8cfef009367a69b6f8526
Varaprasad Maddula <varaprasadmaddula76@gmail.com>
2026-01-28 05:38:20 -0500

    Day 1 commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppointmentListComponent } from './appointments/appointment-list/appointment-list.component';
+import { DoctorListComponent } from './doctors/doctor-list/doctor-list';
+import { PatientListComponent } from './patients/patient-list/patient-list';
+export const routes: Routes = [
+    {
+        path : '', redirectTo : 'login', pathMatch : 'full'
+    },
+    {
+        path : 'login', component : LoginComponent
+    },
+    {
+        path : 'dashboard', component : DashboardComponent
+    },
+    {
+        path : 'patients', component : PatientListComponent
+    },
+    {
+        path : 'doctors', component : DoctorListComponent
+    },
+    {
+        path : 'appointments', component : AppointmentListComponent
+    }
+];
